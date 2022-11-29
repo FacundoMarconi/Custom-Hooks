5e940cc4ab19383c9dbdae6cabeb7a655d3a56e5
Facundo <marconi.facu2000@gmail.com>
2022-11-29 18:14:41 -0300

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [
+  // {
+  //   id: new Date().getTime(),
+  //   description: "Recolectar la piedra de la alma",
+  //   done: false,
+  // },
+];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  return {
+    handleNewTodo,
+    handleDeleteTodo,
+    todos,
+  };
+};
